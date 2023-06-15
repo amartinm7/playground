a2155dabe9070f72e59d07a9d1ff8a8ace55b3b3
antonio martin <antonio.martin@adevinta.com>
2023-06-16 01:15:16 +0200

    update actor examples

--- src/test/kotlin/play/actormodel/Coffeshop.kt
+++ src/test/kotlin/play/actormodel/Coffeshop.kt
@@ -26,6 +26,8 @@ fun main() = runBlocking {
 
     val ordersChannel = Channel<Menu>()
 
+    // we create two coroutines. Every one of them is creating and object passing the channel.
+    // the channel stores the Menu object of every order.
     val spentTime = measureTimeMillis {
         launch(CoroutineName("actor-1")) {
             println("${this.coroutineContext}")
@@ -37,8 +39,12 @@ fun main() = runBlocking {
         }
     }
 
-    // this sentences is for sending the orders to the channel.
-    // there are some actors listening the channel to consume the messages one by one
+    // this sentences are for sending the orders to the channel.
+    // the channel stores the Menu object of every order.
+    // there are some actors listening the channel to consume the messages one by one.
+    // the channel has 1 item capacity in this example.
+    // only an order is sent to the channel when the actor has consumed it and remove it from the channel.
+    // Meanwhile the producer is suspended until the channel is empty.
     launch {
         for (order in orders) {
             delay(200)
