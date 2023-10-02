935f12a0dd094de704f66d4ab188d74bed470d66
antonio martin <antonio.martin@adevinta.com>
2023-10-02 21:36:19 +0200

    react calculator

---
+++ react/projects/calculator/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Calculator from "./pages/Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <p>
+            <code>Calculator</code>
+          </p>
+          <Calculator></Calculator>
+      </header>
+    </div>
+  );
+}
+
+export default App;
