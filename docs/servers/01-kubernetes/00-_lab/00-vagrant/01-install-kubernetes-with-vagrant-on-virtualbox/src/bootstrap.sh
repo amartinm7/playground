aca2067bbb77aec00833fda0d35ed484e1e58a9c
amartinm7 <amartinm7@users.noreply.github.com>
2024-02-04 21:23:53 +0100

    Update bootstrap.sh

--- docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap.sh
+++ docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap.sh
@@ -20,27 +20,15 @@ sudo apt upgrade
 # ```
 
 ## disable swap memory (if this step fails, the installation will fail)
-
-#```bash
-sudo free -h
 sudo swapoff -a
-sudo free -h
-sudo vim /etc/fstab
-## comment the line starts with swap using a #
-
-# or do this
 sudo sed -i 's|/swap.img|#/swap.img|g' /etc/fstab
-sudo sudo sed -i '/ swap / s/^/#/' /etc/fstab
-sudo free -h
 
-sudo reboot
-# ```
-
-## setup host name
+## TODO setup host name
 
 #```bash
 # change host name
-sudo hostnamectl set-hostname "master-node"
+MY_HOST_NAME = "master-node"
+sudo hostnamectl set-hostname $MY_HOST_NAME
 # reload the changes
 sudo exec bash
 #```
@@ -49,12 +37,16 @@ sudo exec bash
 
 #```bash
 # show the ip
-ip --brief addr show
+# ip --brief addr show
 # setup the current ip and the localhost for the master-node
-sudo vim /etc/hosts
+# sudo vim /etc/hosts
 # check the file
-ping master-node
+# ping master-node
 #```
+# find current IP address
+MY_ETH_IP=$(ip a s eth0 | grep -E -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
+# setup "master-node" and current IP
+sudo echo $MY_ETH_IP $MY_HOST_NAME >> /etc/hosts
 
 ## Set up the IPV4 bridge on all nodes
 
@@ -101,7 +93,7 @@ sudo apt-mark hold kubelet kubeadm kubectl
 
 ## Install docker
 #```bash
-sudo apt install docker.io
+sudo apt install -y docker.io
 #```
 
 ## setup containerd
@@ -114,41 +106,24 @@ sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/co
 #```
 ## restart services
 #```bash
+sudo systemctl restart docker.service
+sudo systemctl enable docker.service
 sudo systemctl restart containerd.service
 sudo systemctl enable containerd.service
 sudo systemctl restart kubelet.service
 sudo systemctl enable kubelet.service
 
-sudo systemctl status docker.service
-sudo systemctl status containerd.service
-sudo systemctl status kubelet.service
+# sudo systemctl status docker.service
+# sudo systemctl status containerd.service
+# sudo systemctl status kubelet.service
 #```
-
-## Review the connectivity into the machines
-
-#That is, review the three machines can connect between them. To do that, install the opensshserver
-
-```bash
-# install the tools
-sudo apt install openssh-client
-sudo apt install openssh-server
-
-# test the connectivity
-ssh dell@192.168.0.34
-ssh worker-one@192.168.0.32
-ssh worker-two@192.168.0.31
-#```
-
-## Up to here is the same for the three machines, master and worker nodes
-
----------------
 
 ## Initialize the Kubernetes cluster on the master node (master node only)
 
 #```bash
 sudo kubeadm config images pull
 
-sudo kubeadm init --pod-network-cidr=172.24.0.0/16 --cri-socket=unix:///run/containerd/containerd.sock --upload-certs --control-plane-endpoint=master-node
+sudo kubeadm init --pod-network-cidr=172.24.0.0/16 --cri-socket=unix:///run/containerd/containerd.sock --upload-certs --control-plane-endpoint=$MY_HOST_NAME
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
@@ -175,157 +150,14 @@ kubectl get pods --all-namespaces
 #```bash
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-kubectl get nodes -o wide
-kubectl get pods --all-namespaces
-kubectl cluster-info
-kubectl config view
-kubectl config get-contexts
+# kubectl get nodes -o wide
+# kubectl get pods --all-namespaces
+# kubectl cluster-info
+# kubectl config view
+# kubectl config get-contexts
 
 ## check if the kubelet is not working. If it's not, check the if the swap is disabled.
-sudo systemctl status kubelet.service
-sudo systemctl status docker.service
-sudo systemctl status containerd.service
+# sudo systemctl status kubelet.service
+# sudo systemctl status docker.service
+# sudo systemctl status containerd.service
 #```
-
-## Add worker nodes to the cluster
-#You can now join any number of the control-plane node running the following command on each as root:
-
-#```bash
-  kubeadm join master-node:6443 --token 70fma1.dty00ypu3ubitg2n \
-	--discovery-token-ca-cert-hash sha256:459ff63c64df4bc95cc04a14e91f8b8788fab03cfc6bc7eda41754cfb8a1e316 \
-	--control-plane --certificate-key cd98f8ec2a38193db9ca7736e66da9bde0884fa42e7652a0fdcda9687ab73a7e
-#```
-
-#Please note that the certificate-key gives access to cluster sensitive data, keep it secret!
-#As a safeguard, uploaded-certs will be deleted in two hours; If necessary, you can use
-#"kubeadm init phase upload-certs --upload-certs" to reload certs afterward.
-
-#Then you can join any number of worker nodes by running the following on each as root:
-
-#```bash
-kubeadm join master-node:6443 --token 70fma1.dty00ypu3ubitg2n \
-	--discovery-token-ca-cert-hash sha256:459ff63c64df4bc95cc04a14e91f8b8788fab03cfc6bc7eda41754cfb8a1e316
-#```
-
-## Join the worker nodes
-#First of all create the token and the certificate token on the next steps over the master machine, and return to this point when it was completed
-
-#use the command to join the worker nodes into the worker machine.
-#```bash
-kubeadm join
-#
-sudo kubeadm join master-node:6443 --token $TOKEN --discovery-token-ca-cert-hash sha256:$CERTIFICATE_HASH
-#```
-### Create token
-#Token, launch this command on master server:
-#```bash
-kubeadm token create
-#```
-#If you want to see the active tokens:
-#```bash
-kubeadm token list
-#```
-### Create token certificate
-#Token, launch this command on master server:
-#```bash
-openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'
-#```
-## Install the kubernetes dashboard
-
-#create an user account https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
-#install dashboard https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
-
-#download the dashboard template and install it:
-#```bash
-curl -O https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
-kubectl apply -f recommended.yaml
-#```
-
-### create admin user and secret to store the token
-#```bash
-cat <<EOF | sudo tee dashboard-adminuser.yaml
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: admin-user
-  namespace: kubernetes-dashboard
-EOF
-
-# apply the ServiceAccount template
-kubectl apply -f dashboard-adminuser.yaml
-
-# create ClusterRoleBinding if not exists from the k8s installation
-cat <<EOF | sudo tee ClusterRoleBinding.yaml
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: admin-user
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- kind: ServiceAccount
-  name: admin-user
-  namespace: kubernetes-dashboard
-EOF
-
-# apply the ServiceAccount template
-kubectl apply -f ClusterRoleBinding.yaml
-
-# create the token to login into the dashboard
-kubectl -n kubernetes-dashboard create token admin-user
-
-# create the token and store it into a secret
-cat <<EOF | sudo tee admin-user-secret.yml
-apiVersion: v1
-kind: Secret
-metadata:
-  name: admin-user
-  namespace: kubernetes-dashboard
-  annotations:
-    kubernetes.io/service-account.name: "admin-user"
-type: kubernetes.io/service-account-token
-EOF
-
-# apply the secret template
-kubectl apply -f admin-user-secret.yml
-
-# create the token and store it into the secret
-kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d
-#```
-#execute the next command to enable the proxy an listen on the 8001 port of the current host
-#```bash
-#kubectl proxy
-#```
-
-#check the installation on the next url
-#```bash
-#http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/
-#```
-#access to the dashboard and use the token if it's needed
-#```bash
-#http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/.
-#```
-
-
-## Testing endpoints
-#doing an infinitive loop in single line (sleeping 1sec evrytime)
-#```bash
-#while sleep 1; do curl localhost:8080/v1 && curl localhost:8080/v2; done
-#```
-
-# /etc/fstab: static file system information.
-#
-# Use 'blkid' to print the universally unique identifier for a
-# device; this may be used with UUID= as a more robust way to name devices
-# that works even if disks are added and removed. See fstab(5).
-#
-# <file system> <mount point>   <type>  <options>       <dump>  <pass>
-# / was on /dev/sda2 during curtin installation
-/dev/disk/by-uuid/b2a1e804-89c6-4bce-a130-b7f451d62b29 / ext4 defaults 0 1
-# /boot/efi was on /dev/sda1 during curtin installation
-/dev/disk/by-uuid/63E3-8F5A /boot/efi vfat defaults 0 1
-# /swap.img	none	swap	sw	0	0
-
-
