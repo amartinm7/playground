ccd138cdc32d985cae22910450cc52a0d8c78465
amartinm7 <amartinm7@users.noreply.github.com>
2024-02-05 00:37:49 +0100

    Update bootstrap-worker.sh

--- docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap-worker.sh
+++ docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap-worker.sh
@@ -27,7 +27,7 @@ sudo sed -i 's|/swap.img|#/swap.img|g' /etc/fstab
 
 #```bash
 # change host name
-MY_HOST_NAME = "master-node"
+MY_HOST_NAME="worker"
 sudo hostnamectl set-hostname $MY_HOST_NAME
 # reload the changes
 sudo exec bash
