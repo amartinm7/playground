14ade6f0c3fcaf73d9e7b782d6b8709fabe696b9
amartinm7 <amartinm7@users.noreply.github.com>
2024-02-05 22:20:27 +0100

    Update bootstrap-master.sh

--- docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap-master.sh
+++ docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap-master.sh
@@ -44,9 +44,14 @@ sudo exec bash
 # ping master-node
 #```
 # find current IP address
-MY_ETH_IP=$(ip a s eth0 | grep -E -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
+MY_ETH_IP=$(ip a s eth1 | grep -E -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
 # setup "master-node" and current IP
 sudo echo $MY_ETH_IP $MY_HOST_NAME >> /etc/hosts
+
+## TODO add the woker IP here SETUP
+MY_HOST_WORKER_NAME="worker"
+MY_HOST_WORKER_IP="192.168.56.2"
+sudo echo $MY_HOST_WORKER_IP $MY_HOST_WORKER_NAME >> /etc/hosts
 
 ## Set up the IPV4 bridge on all nodes
 
@@ -150,6 +155,10 @@ kubectl get pods --all-namespaces
 #```bash
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
+## Create a token and sha256 certificate
+sudo kubeadm token create > /vagrant_data/token.txt
+sudo openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' > /vagrant_data/certificate.txt
+
 # kubectl get nodes -o wide
 # kubectl get pods --all-namespaces
 # kubectl cluster-info
@@ -161,3 +170,58 @@ export KUBECONFIG=/etc/kubernetes/admin.conf
 # sudo systemctl status docker.service
 # sudo systemctl status containerd.service
 #```
+
+# Install the kubernetes dashboard
+curl -O https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+kubectl apply -f recommended.yaml
+
+cat <<EOF | sudo tee dashboard-adminuser.yaml
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
+
+# apply the ServiceAccount template
+kubectl apply -f dashboard-adminuser.yaml
+
+# create ClusterRoleBinding if not exists from the k8s installation
+cat <<EOF | sudo tee ClusterRoleBinding.yaml
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: admin-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
+
+# apply the ServiceAccount template
+kubectl apply -f ClusterRoleBinding.yaml
+
+# create the token to login into the dashboard
+kubectl -n kubernetes-dashboard create token admin-user
+
+# create the token and store it into a secret
+cat <<EOF | sudo tee admin-user-secret.yml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: admin-user
+  namespace: kubernetes-dashboard
+  annotations:
+    kubernetes.io/service-account.name: "admin-user"   
+type: kubernetes.io/service-account-token  
+EOF
+
+# apply the secret template
+kubectl apply -f admin-user-secret.yml
+
+# create the token and store it into the secret
+kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d
