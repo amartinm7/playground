d92c0da69b0357edf847fb1720c0a6cf19ad2ce1
amartinm7 <amartinm7@gmail.com>
2024-02-09 19:51:19 +0100

    update documentation

--- docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap-master.sh
+++ docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap-master.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo $1 $2 $3
+
+exit
+
 ## update system
 ./update_system.sh
 
@@ -7,15 +11,15 @@
 ./disable_swap_memory.sh
 
 ## setup host name
-MY_HOST_NAME="master-node"
+MY_HOST_NAME=$1 //"master-node"
 ./setup_host_name $MY_HOST_NAME
 
 ## setup /etc/hosts
 ./setup_etc_hosts_with_eth1_ip.sh $MY_HOST_NAME
 
 ## add the woker-node into the master-node /etc/hosts to get visibility between them
-MY_HOST_NAME_REF="worker"
-MY_HOST_IP_REF="192.168.56.2"
+MY_HOST_NAME_REF=$2 //"worker"
+MY_HOST_IP_REF=$3 //"192.168.56.2"
 ./setup_etc_hosts_with_fixed_ips.sh $MY_HOST_IP_REF $MY_HOST_NAME_REF
 
 ## Set up the IPV4 bridge on all nodes
--- docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap-worker.sh
+++ docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/bootstrap-worker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo $1 $2 $3
+
+exit
 
 ## update system
 ./update_system.sh
@@ -7,15 +10,15 @@
 ./disable_swap_memory.sh
 
 ## setup host name
-MY_HOST_NAME="worker"
+MY_HOST_NAME=$1 //"worker"
 ./setup_host_name $MY_HOST_NAME
 
 ## setup /etc/hosts
 ./setup_etc_hosts_with_eth1_ip.sh $MY_HOST_NAME
 
 ## add the master-node into the worker-node /etc/hosts to get visibility between them
-MY_HOST_NAME_REF="master-node"
-MY_HOST_IP_REF="192.168.56.1"
+MY_HOST_NAME_REF=$2 //"master-node"
+MY_HOST_IP_REF=$3 //"192.168.56.1"
 ./setup_etc_hosts_with_fixed_ips.sh $MY_HOST_IP_REF $MY_HOST_NAME_REF
 
 ## Set up the IPV4 bridge on all nodes
