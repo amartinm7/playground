12aaabfa5525854abe6a65de53248cec06207952
amartinm7 <amartinm7@users.noreply.github.com>
2024-02-10 15:27:19 +0100

    Update create_token_and_sha256_certificate.sh

--- docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/create_token_and_sha256_certificate.sh
+++ docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/create_token_and_sha256_certificate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # creates token and save it into the vagrant shared folder
-sudo kubeadm token create > /vagrant_data/token.txt
+sudo kubeadm token create | sudo tee /vagrant_data/token.txt
 
 # creates sha256 certificate and save it into the vagrant shared folder
-sudo openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' > /vagrant_data/certificate.txt
+sudo openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' | sudo tee /vagrant_data/certificate.txt
