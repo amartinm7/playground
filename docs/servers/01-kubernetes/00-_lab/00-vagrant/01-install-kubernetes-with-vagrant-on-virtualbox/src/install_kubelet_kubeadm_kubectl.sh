cdd98d92a8f732dda256df987b2c4d7e29494a09
amartinm7 <amartinm7@users.noreply.github.com>
2024-02-10 15:29:37 +0100

    Update install_kubelet_kubeadm_kubectl.sh

--- docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/install_kubelet_kubeadm_kubectl.sh
+++ docs/servers/01-kubernetes/00-_lab/00-vagrant/01-install-kubernetes-with-vagrant-on-virtualbox/src/install_kubelet_kubeadm_kubectl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
@@ -10,7 +10,7 @@ curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --de
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt install -y kubelet kubeadm kubectl
 
